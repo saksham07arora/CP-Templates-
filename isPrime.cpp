e0e3aeab0454123b0d13df77fcf0f6098282024c
saksham07arora <116791624+saksham07arora@users.noreply.github.com>
2023-03-13 11:43:39 +0530

    Update isPrime.cpp

--- isPrime.cpp
+++ isPrime.cpp
@@ -10,7 +10,7 @@ bool isPrime(int n) {
         return true;
     if (n % 2 == 0 || n % 3 == 0)
         return false;
-    for (int i = 5; i * i <= n; i = i + 6)
+    for (int i = 5; i * i <= n; i = i + 6)  // The number need to be checked till under-root n
         if (n % i == 0 || n % (i + 2) == 0)
             return false;
     return true;
